f802f8029df7f343be1937f271a7d342825a7ea9
zhangyuehan321 <zhangyuehanv5@126.com>
2026-06-17 17:42:05 +0800

    tailwindcss使用

--- src/pages/Borad/index.tsx
+++ src/pages/Borad/index.tsx
@@ -2,10 +2,10 @@ import { useKanBan } from "@/stores/useKanBan";
 
 export const Board = () => {
     const { borads, createBoard } = useKanBan();
-  return <div>{
+  return <div className="flex flex-col gap-4 bg-red-500">{
     borads.map((board) => (
         <div key={board.id}>{board.name}</div>
     ))}
-    <button onClick={() => createBoard({ id: `{Board.length + 1}`, name: 'Board 1' })}>Create Board</button>
+    <button className="bg-blue-500 text-white p-2 rounded-md" onClick={() => createBoard({ id: `{Board.length + 1}`, name: 'Board 1' })}>Create Board</button>
     </div>
 }
