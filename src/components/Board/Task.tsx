6295c461ef194e3806052ab417039b6dcc3c1abd
zhangyuehan321 <zhangyuehanv5@126.com>
2026-06-18 17:42:56 +0800

    安装拖拽插件

--- src/components/Board/Task.tsx
+++ src/components/Board/Task.tsx
@@ -1,10 +1,23 @@
+import { useDraggable } from "@dnd-kit/core";
 interface TaskProps {   
     title: string;
 }
 
 export const Task = ({ title }: TaskProps) => {
+    const { attributes, listeners, setNodeRef } = useDraggable({
+        id: title,
+        data: {
+            type: 'TASK',//必须要有这个类型
+            title,
+        },
+    });
     return (
-        <div className="p-2 mb-1 rounded-md bg-white border-gray-100">
+        <div
+            {...attributes}
+            {...listeners}
+            ref={setNodeRef}
+            className="p-2 mb-1 rounded-md bg-white border-gray-100"
+        >
             {title}
         </div>
     )
